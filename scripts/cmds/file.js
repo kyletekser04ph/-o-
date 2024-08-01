aa50790031fc39596cf41ea66e9a780ce1395cee
kyletekser04ph <kivlynx42@gmail.com>
2024-08-01 17:43:05 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -2,37 +2,38 @@ const fs = require('fs');
 const { GoatWrapper } = require('fca-liane-utils');
 
 module.exports = {
-	config: {
-		name: "file",
-		version: "1.0",
-		author: "Mahir Tahsan",
-		countDown: 5,
-		role: 0,
-		shortDescription: "Send bot script",
-		longDescription: "Send bot specified file ",
-		category: "𝗢𝗪𝗡𝗘𝗥",
-		guide: "{pn} file name. Ex: .{pn} filename"
-	},
+  config: {
+    name: "file",
+    version: "1.0",
+    author: "Kyle",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "owner",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
 
-	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61556771164358"];
-		if (!permission.includes(event.senderID)) {
-			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
-		}
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["100052395031835","61561428180031"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("⛔ 𝗔𝗖𝗖𝗘𝗦𝗦 𝗗𝗘𝗡𝗜𝗘𝗗\n\n𝖸𝖮𝖴 𝖣𝖮𝖭'𝖳 𝖧𝖠𝖵𝖤 𝖤𝖭𝖮𝖴𝖦𝖧 𝖯𝖤𝖱𝖬𝖨𝖲𝖲𝖨𝖮𝖭 𝖳𝖮 𝖴𝖲𝖤𝖣 𝖳𝖧𝖨𝖲 𝖢𝖮𝖬𝖬𝖠𝖭𝖣𝖲, 𝖮𝖭𝖫𝖸 𝖬𝖸 𝖡𝖮𝖲𝖲 Kyle敦. ဗီူ  𝖢𝖠𝖭 𝖣𝖮 𝖨𝖳(⋋▂⋌)", event.threadID, event.messageID);
+    }
 
-		const fileName = args[0];
-		if (!fileName) {
-			return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
-		}
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("put the file name there bit*h(⋋▂⋌)🖕", event.threadID, event.messageID);
+    }
 
-		const filePath = __dirname + `/${fileName}.js`;
-		if (!fs.existsSync(filePath)) {
-			return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
-		}
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`🚫 𝗡𝗼𝘁 𝗙𝗼𝘂𝗻𝗱 [✖]: ${fileName}.js`, event.threadID, event.messageID);
+    }
 
-		const fileContent = fs.readFileSync(filePath, 'utf8');
-		api.sendMessage({ body: fileContent }, event.threadID);
-	}
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
 };
+
 const wrapper = new GoatWrapper(module.exports);
 wrapper.applyNoPrefix({ allowPrefix: true });
